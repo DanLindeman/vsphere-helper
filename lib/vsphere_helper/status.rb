1513d518574bc903f289bcd3bbb0dfb5001e0471
Mark Arbogast <mark.arbogast@bluemedora.com>
2013-10-07 10:39:45 -0400

    Removed duplicate recursive_find_vm function from status.

--- lib/vsphere_helper/status.rb
+++ lib/vsphere_helper/status.rb
@@ -7,16 +7,3 @@ def status(name)
   vms_found = recursive_find_vm(data_center.vmFolder,name)
   vms_found.each { |vm| puts "#{vm.name}: #{vm.summary.runtime.powerState}" }
 end
-
-def recursive_find_vm(folder, name)
-  found = []
-  folder.children.each do |child|
-    if ((child.class == RbVmomi::VIM::VirtualMachine) && (child.name.include? name))
-      found << child
-    elsif child.class == RbVmomi::VIM::Folder
-      found << recursive_find_vm(child,name)
-    end
-  end
-
-  found.flatten
-end
