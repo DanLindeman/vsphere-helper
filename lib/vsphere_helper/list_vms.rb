5c6653b621a727798fd4b7493dcf22aefc1e46b3
Mark Arbogast <mark.arbogast@bluemedora.com>
2013-10-07 11:48:34 -0400

    List_vms now uses find_vm with the '*' wildcard instead of its own (very similar) function.

--- lib/vsphere_helper/list_vms.rb
+++ lib/vsphere_helper/list_vms.rb
@@ -3,12 +3,6 @@ require 'rbvmomi'
 def list_vms
   connection = connect
   data_center = connection.serviceInstance.find_datacenter
-  recursive_list_vms data_center.vmFolder
-end
-
-def recursive_list_vms(folder)
-  folder.children.each do |child|
-    puts child.name if child.class == RbVmomi::VIM::VirtualMachine
-    recursive_list_vms child if child.class == RbVmomi::VIM::Folder
-  end
+  all_vms = find_vm("*")
+  all_vms.each { |vm| vm.name }
 end
