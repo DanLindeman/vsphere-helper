07e8358d934bf7cb5578eb98ecad0ee6d0e4e77c
Mark Arbogast <mark.arbogast@bluemedora.com>
2013-10-08 15:20:47 -0400

    Simplified list_vms to use find_vm.

--- lib/vsphere_helper/list_vms.rb
+++ lib/vsphere_helper/list_vms.rb
@@ -3,6 +3,5 @@ require 'rbvmomi'
 def list_vms
   connection = connect
   data_center = connection.serviceInstance.find_datacenter
-  all_vms = find_vm("*")
-  all_vms.each { |vm| vm.name }
+  find_vm("*")
 end
