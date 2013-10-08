9a03ae4f2ab6138610dee9fd1385749161220306
Mark Arbogast <mark.arbogast@bluemedora.com>
2013-10-08 16:21:23 -0400

    Added support to find_vm for exact matching.

--- lib/vsphere_helper/find_vm.rb
+++ lib/vsphere_helper/find_vm.rb
@@ -1,28 +1,28 @@
 require 'rbvmomi'
 
-def find_vm(name)
+def find_vm(name, exact = false)
   name = name.first if name.kind_of?(Array)
   connection = connect
   data_center = connection.serviceInstance.find_datacenter
-  vms_found = recursive_find_vm(data_center.vmFolder,name)
+  vms_found = recursive_find_vm(data_center.vmFolder, name, exact)
   vms_found.each { |vm| puts vm.name }
 end
 
-def recursive_find_vm(folder, name)
+def recursive_find_vm(folder, name, exact = false)
   found = []
   folder.children.each do |child|
-    if matches(child, name)
+    if matches(child, name, exact)
       found << child
     elsif child.class == RbVmomi::VIM::Folder
-      found << recursive_find_vm(child,name)
+      found << recursive_find_vm(child, name, exact)
     end
   end
 
   found.flatten
 end
 
-def matches(child, name)
+def matches(child, name, exact = false)
   is_vm = child.class == RbVmomi::VIM::VirtualMachine
-  name_matches = ((name == "*") || (child.name.include? name))
+  name_matches = (name == "*") || (exact ? (child.name == name) : (child.name.include? name))
   return is_vm && name_matches
 end
