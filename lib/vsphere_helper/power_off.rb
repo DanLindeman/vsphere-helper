6bd3e19f2b768b58e21bb580b5125d88c1dee7a7
Mark Arbogast <mark.arbogast@bluemedora.com>
2013-10-09 14:42:22 -0400

    Added power_on and power_off.

---
+++ lib/vsphere_helper/power_off.rb
@@ -0,0 +1,11 @@
+require 'rbvmomi'
+
+def power_off(name)
+  name = name.first
+  connection = connect
+  data_center = connection.serviceInstance.find_datacenter
+  vm = recursive_find_vm(data_center.vmFolder, name, true).first or die("No vm found with name matching <#{name}>")
+  puts "Powering off #{vm.name}..."
+  vm.PowerOffVM_Task.wait_for_completion
+  puts "#{vm.name} is now powered off."
+end
---
+++ lib/vsphere_helper/power_on.rb
@@ -0,0 +1,11 @@
+require 'rbvmomi'
+
+def power_on(name)
+  name = name.first
+  connection = connect
+  data_center = connection.serviceInstance.find_datacenter
+  vm = recursive_find_vm(data_center.vmFolder, name, true).first or die("No vm found with name matching <#{name}>")
+  puts "Powering on #{vm.name}..."
+  vm.PowerOnVM_Task.wait_for_completion
+  puts "#{vm.name} is now powered on."
+end
